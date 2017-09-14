c7580fc6c47c2cf50ccb60185d5ca4ce1e4dee32
rentzhx3 <rentzhx3@gmail.com>
2017-09-14 02:16:31 -0400

    support more series

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+var BASE_NAME = "/" + window.location.pathname.split('/')[1];
+
+
+if (BASE_NAME === "/" || BASE_NAME === "/index.html") {
+    BASE_NAME = "/HunterXHunter";
+}
+
+var INFO_PATH = BASE_NAME + "/info.json";
+var ISSUES_PATH = BASE_NAME + "/issues.json";
+
+
 var app = new Vue({
         el: '#app',
         data: {
@@ -28,18 +39,7 @@ var app = new Vue({
                 }
             },
             show_arcs: false,
-            "arcs": [
-                {"key": "hiatus-arc", "name": "Great Hiatus Arc"},
-                {"key": "hunter-exam", "name": "Hunter Exam"},
-                {"key": "zoldyck", "name": "Zoldyck Family"},
-                {"key": "heavens-arena", "name": "Heavens Arena"},
-                {"key": "yorknew", "name": "Yorknew City"},
-                {"key": "greed-island", "name": "Greed Island"},
-                {"key": "chimera-ant", "name": "Chimera Ant"},
-                {"key": "election", "name": "13th Hunter Chairman Election"},
-                {"key": "dc", "name": "Dark Continent Expedition"},
-                {"key": "oneshot", "name": "Kurapika's One Shot"}
-            ],
+            info: {},
             major_hiatuses: []
         },
         computed: {
@@ -87,155 +87,167 @@ var app = new Vue({
         },
         mounted: function () {
             var that = this;
-            axios.get("issues.json")
+            axios.get(INFO_PATH)
                 .then(function (response) {
-                    that.all_issues = response.data;
+                    that.info = response.data;
+                    document.title = that.info.series_name + " Hiatus Chart";
+                }).then(function () {
+                axios.get(ISSUES_PATH)
+                    .then(function (response) {
+                        that.all_issues = response.data;
 
-                    var count = 0;
-                    var countPresent = 0;
+                        var count = 0;
+                        var countPresent = 0;
 
-                    var currentYear = 0;
-                    var currentYearPresent = 0;
-                    var initIssue = 0;
-                    var initIssuePresent = 0;
+                        var currentYear = 0;
+                        var currentYearPresent = 0;
+                        var initIssue = 0;
+                        var initIssuePresent = 0;
 
-                    var lastIssue;
+                        var lastIssue;
 
 
-                    _.sortBy(that.all_issues, ['year', 'number']).forEach(function (issue) {
-                        that.status.overall.totalIssues++;
-                        if (!issue.released) {
-                            that.status.overall.totalHiatus++;
-                            if (countPresent > that.status.pubStreak.count) {
-                                that.status.pubStreak.count = countPresent;
-                                that.status.pubStreak.begin.year = currentYearPresent;
-                                that.status.pubStreak.begin.issue = initIssuePresent;
-                                that.status.pubStreak.end.year = lastIssue.year;
-                                that.status.pubStreak.end.issue = lastIssue.number;
+                        _.sortBy(that.all_issues, ['year', 'number']).forEach(function (issue) {
+                            that.status.overall.totalIssues++;
+                            if (!issue.released) {
+                                that.status.overall.totalHiatus++;
+                                if (countPresent > that.status.pubStreak.count) {
+                                    that.status.pubStreak.count = countPresent;
+                                    that.status.pubStreak.begin.year = currentYearPresent;
+                                    that.status.pubStreak.begin.issue = initIssuePresent;
+                                    that.status.pubStreak.end.year = lastIssue.year;
+                                    that.status.pubStreak.end.issue = lastIssue.number;
+                                }
+                                countPresent = 0;
+
+
+                                if (count === 0) {
+                                    currentYear = issue.year;
+                                    initIssue = issue.number;
+                                }
+                                count++;
+
+
                             }
-                            countPresent = 0;
+                            else {
+                                that.status.overall.totalPresent++;
+                                if (count > that.status.longest.hiatusCount) {
+                                    that.status.longest.hiatusCount = count;
+                                    that.status.longest.begin.year = currentYear;
+                                    that.status.longest.begin.issue = initIssue;
+                                    that.status.longest.end.year = lastIssue.year;
+                                    that.status.longest.end.issue = lastIssue.number;
+
+                                }
+                                count = 0;
 
 
-                            if (count === 0) {
-                                currentYear = issue.year;
-                                initIssue = issue.number;
+                                if (countPresent === 0) {
+                                    currentYearPresent = issue.year;
+                                    initIssuePresent = issue.number;
+                                }
+                                countPresent++;
                             }
-                            count++;
+                            lastIssue = issue;
+                        });
 
 
+                        that.status.current.hiatusCount = count;
+                        that.status.current.begin.issue = initIssue;
+                        that.status.current.begin.year = currentYear;
+
+                        if (that.status.current.hiatusCount > that.status.longest.hiatusCount) {
+                            that.status.longest.hiatusCount = that.status.current.hiatusCount;
+                            that.status.longest.begin.year = that.status.current.begin.year;
+                            that.status.longest.begin.issue = that.status.current.begin.issue;
+                            that.status.longest.end.year = lastIssue.year;
+                            that.status.longest.end.issue = lastIssue.number;
                         }
-                        else {
-                            that.status.overall.totalPresent++;
-                            if (count > that.status.longest.hiatusCount) {
-                                that.status.longest.hiatusCount = count;
-                                that.status.longest.begin.year = currentYear;
-                                that.status.longest.begin.issue = initIssue;
-                                that.status.longest.end.year = lastIssue.year;
-                                that.status.longest.end.issue = lastIssue.number;
 
-                            }
-                            count = 0;
+                        that.status.currentPubStreak.count = countPresent;
+                        that.status.currentPubStreak.begin.issue = initIssuePresent;
+                        that.status.currentPubStreak.begin.year = currentYearPresent;
 
-
-                            if (countPresent === 0) {
-                                currentYearPresent = issue.year;
-                                initIssuePresent = issue.number;
-                            }
-                            countPresent++;
+                        if (that.status.currentPubStreak.count > that.status.pubStreak.count) {
+                            that.status.pubStreak.count = that.status.currentPubStreak.count;
+                            that.status.pubStreak.begin.year = that.status.currentPubStreak.begin.year;
+                            that.status.pubStreak.begin.issue = that.status.currentPubStreak.begin.issue;
+                            that.status.pubStreak.end.year = lastIssue.year;
+                            that.status.pubStreak.end.issue = lastIssue.number;
                         }
-                        lastIssue = issue;
-                    });
+
+                        that.status.arcs = [];
 
 
-                    that.status.current.hiatusCount = count;
-                    that.status.current.begin.issue = initIssue;
-                    that.status.current.begin.year = currentYear;
-
-                    if (that.status.current.hiatusCount > that.status.longest.hiatusCount) {
-                        that.status.longest.hiatusCount = that.status.current.hiatusCount;
-                        that.status.longest.begin.year = that.status.current.begin.year;
-                        that.status.longest.begin.issue = that.status.current.begin.issue;
-                        that.status.longest.end.year = lastIssue.year;
-                        that.status.longest.end.issue = lastIssue.number;
-                    }
-
-                    that.status.currentPubStreak.count = countPresent;
-                    that.status.currentPubStreak.begin.issue = initIssuePresent;
-                    that.status.currentPubStreak.begin.year = currentYearPresent;
-
-                    if (that.status.currentPubStreak.count > that.status.pubStreak.count) {
-                        that.status.pubStreak.count = that.status.currentPubStreak.count;
-                        that.status.pubStreak.begin.year = that.status.currentPubStreak.begin.year;
-                        that.status.pubStreak.begin.issue = that.status.currentPubStreak.begin.issue;
-                        that.status.pubStreak.end.year = lastIssue.year;
-                        that.status.pubStreak.end.issue = lastIssue.number;
-                    }
-
-                    that.status.arcs = [];
-
-                    that.status.currentWidth = (that.status.current.hiatusCount / that.status.longest.hiatusCount / 0.01) + "%";
-                    that.status.pubStreakWidth = (that.status.pubStreak.count / that.status.longest.hiatusCount / 0.01) + "%";
-                    that.status.currentPubStreakWidth = (that.status.currentPubStreak.count / that.status.longest.hiatusCount / 0.01) + "%";
-                    that.status.pTotalPresent = (that.status.overall.totalPresent / that.status.overall.totalIssues / 0.01) + "%";
-                    that.status.pTotalHiatus = (that.status.overall.totalHiatus / that.status.overall.totalIssues / 0.01) + "%";
+                        var largest = Math.max(that.status.current.hiatusCount, that.status.longest.hiatusCount, that.status.pubStreak.count);
 
 
-                    // extract major hiatuses
+                        that.status.currentWidth = (that.status.current.hiatusCount / largest / 0.01) + "%";
+                        that.status.longestWidth = (that.status.longest.hiatusCount / largest / 0.01) + "%";
+                        that.status.pubStreakWidth = (that.status.pubStreak.count / largest / 0.01) + "%";
+                        that.status.currentPubStreakWidth = (that.status.currentPubStreak.count / largest / 0.01) + "%";
+                        that.status.pTotalPresent = (that.status.overall.totalPresent / that.status.overall.totalIssues / 0.01) + "%";
+                        that.status.pTotalHiatus = (that.status.overall.totalHiatus / that.status.overall.totalIssues / 0.01) + "%";
 
-                    var last_issue = {};
-                    var hiatus_count = 0;
-                    var last_begin = {};
 
-                    var hiatuses = [];
-                    var greatest = 0;
-                    _.sortBy(that.all_issues, ['year', 'number']).forEach(function (issue) {
-                        if (last_issue) {
-                            if (last_issue.released && !issue.released) {
-                                hiatus_count = 0;
-                                last_begin = issue;
-                            }
-                            else if (!last_issue.released && issue.released) {
-                                if (hiatus_count > 4) {
-                                    hiatuses.push({
-                                        start: last_begin.year + '/' + last_begin.number,
-                                        end: last_issue.year + '/' + last_issue.number,
-                                        total: hiatus_count
-                                    });
-                                    if (hiatus_count > greatest) {
-                                        greatest = hiatus_count;
+                        // extract major hiatuses
+
+                        var last_issue = {};
+                        var hiatus_count = 0;
+                        var last_begin = {};
+
+                        var hiatuses = [];
+                        var greatest = 0;
+                        _.sortBy(that.all_issues, ['year', 'number']).forEach(function (issue) {
+                            if (last_issue) {
+                                if (last_issue.released && !issue.released) {
+                                    hiatus_count = 0;
+                                    last_begin = issue;
+                                }
+                                else if (!last_issue.released && issue.released) {
+                                    if (hiatus_count > that.info.major_hiatus_threshold) {
+                                        hiatuses.push({
+                                            start: last_begin.year + '/' + last_begin.number,
+                                            end: last_issue.year + '/' + last_issue.number,
+                                            total: hiatus_count
+                                        });
+                                        if (hiatus_count > greatest) {
+                                            greatest = hiatus_count;
+                                        }
                                     }
+
                                 }
 
                             }
-
-                        }
-                        last_issue = issue;
-                        if (!issue.released) {
-                            hiatus_count++;
-                        }
-                    });
-
-                    if (!last_issue.released && hiatus_count > 4) {
-                        hiatuses.push({
-                            start: last_begin.year + '/' + last_begin.number,
-                            end: '??',
-                            total: hiatus_count
+                            last_issue = issue;
+                            if (!issue.released) {
+                                hiatus_count++;
+                            }
                         });
-                        if (hiatus_count > greatest) {
-                            greatest = hiatus_count;
+
+                        if (!last_issue.released && hiatus_count >= that.info.major_hiatus_threshold) {
+                            hiatuses.push({
+                                start: last_begin.year + '/' + last_begin.number,
+                                end: '??',
+                                total: hiatus_count
+                            });
+                            if (hiatus_count > greatest) {
+                                greatest = hiatus_count;
+                            }
                         }
-                    }
 
-                    for (var i = 0; i < hiatuses.length; i++) {
-                        hiatuses[i].viewProportion = (hiatuses[i].total / greatest / 0.01) + "%";
-                    }
+                        for (var i = 0; i < hiatuses.length; i++) {
+                            hiatuses[i].viewProportion = (hiatuses[i].total / greatest / 0.01) + "%";
+                        }
 
-                    that.status.major_hiatuses = hiatuses;
+                        that.status.major_hiatuses = hiatuses;
 
-                    that.initGraph(that.$refs.yearlyCanvas, 'line')
+                        that.initGraph(that.$refs.yearlyCanvas, 'line')
 
-                })
-            ;
+                    })
+                ;
+            });
+
 
         },
         methods: {
@@ -256,7 +268,7 @@ var app = new Vue({
                 var dataset = data.map(mapToTotalReleased);
 
 
-                var chart = new Chart(canvas, {
+                new Chart(canvas, {
                     type: type,
                     data: {
                         labels: labels,
