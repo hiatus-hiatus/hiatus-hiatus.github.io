54cdff00050b8374c3807adbe43ab34a7d2f8c88
rentzhx3 <rentzhx3@gmail.com>
2020-01-14 02:24:43 -0400

    create basic main chart

---
+++ src/components/Issue.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="issue-square"
+    :class="{
+      released: issue.released,
+      hiatus: !issue.released
+    }"
+  >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Issue",
+  props: {
+    issue: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.issue-square {
+  width: 21px;
+  height: 16px;
+  background: #ff4444;
+}
+.released {
+  background: #0099cc;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,86 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div class="chart-row" v-for="year in issues_by_year">
+      <div class="year">
+        {{ year.year }}
+      </div>
+      <issue v-for="issue in year.issues" :issue="issue" class="issue" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+import Issue from "@/components/Issue";
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  components: { Issue },
+
+  data() {
+    return {
+      loading: false,
+      series_name: "",
+      ongoing: false,
+      major_hiatus_threshold: 0,
+      arcs: [],
+      issues: []
+    };
+  },
+  created() {
+    this.load();
+  },
+  methods: {
+    async load() {
+      this.loading = true;
+      try {
+        const response = await axios.get("/HunterXHunter/info.json");
+        const data = response.data;
+        this.series_name = data.series_name;
+        this.issues = data.issues.reverse();
+      } finally {
+        this.loading = false;
+      }
+    }
+  },
+  computed: {
+    issues_by_year() {
+      const grouped_by_year = {};
+
+      for (const issue of this.issues) {
+        if (!grouped_by_year[issue.year]) {
+          grouped_by_year[issue.year] = [];
+        }
+        grouped_by_year[issue.year].push(issue);
+      }
+
+      const data = [];
+      for (const year of Object.keys(grouped_by_year).reverse()) {
+        data.push({ year, issues: grouped_by_year[year] });
+      }
+      console.log(data);
+      return data;
+    }
   }
 };
 </script>
+<style lang="scss" scoped>
+.year {
+  padding: 0;
+  display: flex;
+  vertical-align: top;
+  margin-right: 4px;
+  font-size: 12px;
+  width: 25px;
+}
+.chart-row {
+  display: flex;
+  margin-bottom: 2px;
+  .issue {
+    margin-left: 1px;
+    margin-right: 1px;
+  }
+}
+</style>
