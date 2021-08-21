2e013a57e1dd2e44864d7a733d7325d860f9437e
rentzhx3 <rentzhx3@gmail.com>
2021-08-21 04:23:22 -0400

    add cdn aliases to build

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,26 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    rollupOptions: {
-      external: ["vue"],
+export default defineConfig(({ mode }) => {
+  let aliases = {};
+  if (mode == "production") {
+    aliases = {
+      vue: "https://cdn.jsdelivr.net/npm/vue@3.2.4/dist/vue.runtime.esm-browser.prod.js",
+      luxon: "https://cdn.jsdelivr.net/npm/luxon@2.0.2/build/es6/luxon.js",
+      "chart.js":
+        "https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.esm.js",
+    };
+  }
+  return {
+    plugins: [vue()],
+    base: "/chart-tests",
+    build: {
+      rollupOptions: {},
     },
-  },
+    resolve: {
+      alias: {
+        ...aliases,
+      },
+    },
+  };
 });
